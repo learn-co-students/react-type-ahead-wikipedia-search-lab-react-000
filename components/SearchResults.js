e0abab1d12619e8e45db46d647fd0f6afeda800d
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-11-07 09:53:15 -0500

    SearchResults done

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -4,9 +4,19 @@ const React = require('react');
 
 class SearchResults extends React.Component {
   render(){
+    const { results } = this.props;
     return(
-      <ul>
-        
+      <ul className="search-results">
+        {results.map(r => {
+          return (
+            <li>
+              <a href={r.link}>{r.title}</a>
+              <p>
+                {r.description}
+              </p>
+            </li>
+          )
+        })}
       </ul>
     )
   }
